a34b89ec3343984731878b07e68b1bdf18e7edaa
Jiri Hon <jiri.hon@gmail.com>
2017-01-18 11:23:22 +0000

    Setting min_score to 27.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pqsfinder@126002 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/pqsfinder.cpp
+++ src/pqsfinder.cpp
@@ -730,7 +730,7 @@ SEXP pqsfinder(
     std::string strand = "*",
     bool overlapping = false,
     int max_len = 50,
-    int min_score = 42,
+    int min_score = 27,
     int run_min_len = 2,
     int run_max_len = 11,
     int loop_min_len = 1,
