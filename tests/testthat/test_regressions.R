6d5f975100c9e9bd2a53512e80c606b2862ccab9
Jiri Hon <jiri.hon@gmail.com>
2017-06-18 17:45:19 +0000

    Updated regression test to give more detailed output. Version bump.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pqsfinder@130498 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_regressions.R
+++ tests/testthat/test_regressions.R
@@ -14,6 +14,10 @@ test_that("the result on test seq is the same as gives pqsfinder-1.4.4", {
   pv_d <- pqsfinder(test_seq, strand = "+")
   pv_r <- pqsfinder(test_seq, strand = "+", run_re = "G{1,10}.{0,10}G{1,10}")
   
+  library(rtracklayer)
+  export(as(pv_d, "GRanges"), "pv_d.gff", version = "3")
+  cat(readLines("pv_d.gff"), sep = "\n")
+  
   cat("pv_d, pv_r\n")
   expect_equal_pv(pv_d, pv_r)
   cat("pqsfinder_1_4_4_d, pqsfinder_1_4_4_r\n")
