7db1152a4d3d18d724975aba00b0948b05624d84
Jiri Hon <jiri.hon@gmail.com>
2019-10-10 14:39:54 +0200

    Use int rather than char to avoid overflow when user will search for extremely long motifs.

--- src/features.h
+++ src/features.h
@@ -10,15 +10,15 @@
 #define FEATURES_HEADER
 
 struct features_t {
-  char nt = 0;
-  char nb = 0;
-  char nm = 0;
-  char rl1 = 0;
-  char rl2 = 0;
-  char rl3 = 0;
-  char ll1 = 0;
-  char ll2 = 0;
-  char ll3 = 0;
+  int nt = 0;
+  int nb = 0;
+  int nm = 0;
+  int rl1 = 0;
+  int rl2 = 0;
+  int rl3 = 0;
+  int ll1 = 0;
+  int ll2 = 0;
+  int ll3 = 0;
 };
 
 #endif // FEATURES_HEADER
